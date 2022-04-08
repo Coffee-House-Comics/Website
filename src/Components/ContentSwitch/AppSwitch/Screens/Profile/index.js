852e878d6c4a676ee2022b1e97aa1f40335183f9
Shaan Khan <contactshaankhan@gmail.com>
2022-04-08 16:07:32 -0400

    Profile General Layout is complete.

--- src/Components/ContentSwitch/AppSwitch/Screens/Profile/index.js
+++ src/Components/ContentSwitch/AppSwitch/Screens/Profile/index.js
@@ -27,6 +27,11 @@ function ProfileRouter() {
 
     const [profileTab, setProfileTab] = useState(PROFILE_TABS.VIEW);
 
+    function changeTab(tab) {
+        console.log("Changing to..:", tab);
+        setProfileTab(tab);
+    }
+
     // Get the active Screen
     let activeScreen = <View />;
     if (profileTab === PROFILE_TABS.FORUM)
@@ -36,6 +41,25 @@ function ProfileRouter() {
     else if (profileTab === PROFILE_TABS.SETTINGS)
         activeScreen = <Settings />;
 
+    const lineCss = "3px solid " + theme.palette.coffee.main;
+
+    function backgroundCSS(tab) {
+        return {
+            cursor: "pointer",
+            textAlign: "center",
+            borderBottom: lineCss,
+            borderRadius: "5px 5px 0px 0px",
+            padding: "10px 0px 10px 0px",
+            bgcolor: (profileTab === tab) ? theme.palette.coffee.main : "transparent",
+            color: (profileTab === tab) ? theme.palette.ivory.main : theme.palette.olive_drab_7.main
+        }
+    }
+
+    function mutateText(text) {
+        return (
+            <Typography variant="h6" >{text}</Typography>
+        );
+    }
 
     return (
         <Box sx={{
@@ -50,17 +74,57 @@ function ProfileRouter() {
                 Put Profile Card here
             </Box>
             <Box sx={{
-                bgcolor: theme.palette.fuzzy_wuzzy.main,
-                height: "100%"
+                // bgcolor: theme.palette.fuzzy_wuzzy.main,
+                height: "100%",
+                width: "calc(100% - 350px)",
+                float: "right"
             }}>
-                {"View Profile with id: " + id}
-                <button onClick={() => setProfileTab(PROFILE_TABS.VIEW)}>VIEW</button>
-                <button onClick={() => setProfileTab(PROFILE_TABS.SETTINGS)}>SETTINGS</button>
-                <button onClick={() => setProfileTab(PROFILE_TABS.SAVED)}>SAVED</button>
-                <button onClick={() => setProfileTab(PROFILE_TABS.FORUM)}>FORUM</button>
-                {activeScreen}
-            </Box>
+                <Grid container spacing={0} sx={{
+                    position: "relative",
+                    padding: "20px",
+                }}>
+                    <Grid item xs={1} sx={{
+                        borderBottom: lineCss,
+                        padding: "10px 0px 10px 0px",
+                    }} />
+                    <Grid item xs={2}
+                        onClick={() => changeTab(PROFILE_TABS.VIEW)}
+                        sx={{
+                            ...backgroundCSS(PROFILE_TABS.VIEW),
 
+                        }}>
+                        {mutateText("View")}
+                    </Grid>
+                    <Grid item xs={2}
+                        onClick={() => changeTab(PROFILE_TABS.FORUM)}
+                        sx={{
+                            ...backgroundCSS(PROFILE_TABS.FORUM),
+                        }}>
+                        {mutateText("Forum")}
+                    </Grid>
+                    <Grid item xs={2}
+                        onClick={() => changeTab(PROFILE_TABS.SAVED)}
+                        sx={{
+                            ...backgroundCSS(PROFILE_TABS.SAVED),
+                        }}>
+                        {mutateText("Saved")}
+                    </Grid>
+                    <Grid item xs={2}
+                        onClick={() => changeTab(PROFILE_TABS.SETTINGS)}
+                        sx={{
+                            ...backgroundCSS(PROFILE_TABS.SETTINGS),
+                        }}>
+                        {mutateText("Settings")}
+                    </Grid>
+                    <Grid item xs={3} sx={{
+                        borderBottom: lineCss,
+                        padding: "10px 0px 10px 0px",
+                    }} />
+                    <Grid item xs={12}>
+                        {activeScreen}
+                    </Grid>
+                </Grid>
+            </Box>
         </Box>
     );
 }
