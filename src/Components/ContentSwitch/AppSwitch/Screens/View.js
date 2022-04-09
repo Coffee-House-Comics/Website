aac6860feed72b6569ce054cd48b72adf0be30d0
mattjb01 <matthew.broadbent@stonybrook.edu>
2022-04-08 22:18:10 -0400

    Initial View component

--- src/Components/ContentSwitch/AppSwitch/Screens/View.js
+++ src/Components/ContentSwitch/AppSwitch/Screens/View.js
@@ -1,10 +1,23 @@
 import { useParams } from 'react-router-dom';
+import { Grid, Image, Typography, ThemeProvider, Avatar } from '@mui/material';
 
 function View() {
     // Get the id that we want to view
     const { id } = useParams();
 
-    return (<div>{"Trying to view content with id: " + id}</div>);
+    let tab = "content"; //TODO content or comments depending on store
+    let  mode = "comic"; // TODO comic or story depending on store
+
+    return (<div>
+        <Grid container direction="row" justifyContent="flex-start" alignItems="center" spacing={2}>
+            <Grid item>
+                {mode == "content"? <MetadataPanel id={id}/>: <CommentsPanel id={id}/>}
+            </Grid>
+            <Grid item>
+                <ContentPanel id={id}></ContentPanel>
+            </Grid>
+        </Grid>
+    </div>);
 
 }
 
