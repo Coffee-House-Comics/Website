cf7685e9eaca2a4326a21d204b3c24d46ca26a06
mattjb01 <matthew.broadbent@stonybrook.edu>
2022-04-21 17:34:07 -0400

    Functions in comic creation store

--- src/Store/ComicCreationStore.js
+++ src/Store/ComicCreationStore.js
@@ -1,6 +1,8 @@
-import { createContext, useState, useEffect, useRef } from 'react';
+import { createContext, useState, useEffect, useContext, useRef } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import types from '../Common/Types';
+import { GlobalStoreContext } from '.';
+import API from '../API';
 
 const ComicStoreContext = createContext({});
 
@@ -8,11 +10,15 @@ const ComicStoreActionType = {
     SET_CURRENT_PAGE: "SET_CURRENT_PAGE"
 }
 
+const { mainStore } = useContext(GlobalStoreContext);
+
+const ComicAPI = API.Comic;
+
 //Setting up comic store
 function ComicStoreContextProvider(props) {
 
     const [store, setStore] = useState({
-        comic: null,
+        comicId: null,
 
         currentPage: {
             currentTool: "pen",
@@ -27,7 +33,7 @@ function ComicStoreContextProvider(props) {
         switch (type) {
             case ComicStoreActionType.SET_CURRENT_PAGE: {
                 return setStore({
-                    comic: store.comic,
+                    comicId: store.comicId,
                     currentPage: payload
                 });
             }
@@ -36,24 +42,66 @@ function ComicStoreContextProvider(props) {
         }
     }
 
-    store.updateKonvaJSON = async function (json) {
-
-    }
-
-    store.changeCurrentTab = function() {
-
+    store.changeCurrentTab = function(newTab) {
+        storeReducer({
+            type: ComicStoreActionType.SET_CURRENT_PAGE,
+            payload: {
+                currentTool: store.currentPage.currentTool,
+                currentTab: newTab,
+                konvaJson: store.currentPage.konvaJson
+            }
+        });
     }
 
     store.changeCurrentTool = function(newTool) {
+        storeReducer({
+            type: ComicStoreActionType.SET_CURRENT_PAGE,
+            payload: {
+                currentTool: newTool,
+                currentTab: store.currentPage.currentTab,
+                konvaJson: store.currentPage.konvaJson
+            }
+        });
+    }
 
+    store.updateKonvaJSON = function (json) {
+        storeReducer({
+            type: ComicStoreActionType.SET_CURRENT_PAGE,
+            payload: {
+                currentTool: store.currentPage.currentTool,
+                currentTab: store.currentPage.currentTab,
+                konvaJson: json
+            }
+        });
     }
 
     store.loadPage = async function() {
 
     }
 
-    store.updateMetaData = async function(newData) {
+    store.editMetaData = async function(newData) {
+        const { name, description, coverPhoto, series } = newData;
 
+        try {
+            mainStore.toggleLoading();
+            const response = await ComicAPI.editMetaData(store.comicId, name, description, coverPhoto, series);
+            mainStore.toggleLoading();
+
+            if(response.status === 200) {
+                mainStore.reRoute(types.TabType.CREATION.children.COMIC.fullRoute.slice(0, -3) + store.comicId);
+            }
+        }
+
+        catch {
+
+        }
+
+        console.log("Couldn't edit metadata :/");
+        mainStore.createModal({
+            title: "Error editing metadata",
+            body: "Comic metadata could not be edited. Please try again.",
+            action: ""
+        });
     }
 
     store.undo = async function() {
