7d8bc8a7ebf6f7473d517cb22db6eecaf612975a
taylor-giles <taylor.g1823@gmail.com>
2022-04-23 01:09:41 -0400

    Add uploadFile function

--- src/Utils.js
+++ src/Utils.js
@@ -6,10 +6,14 @@ Utils.uploadFileFromInput = async function(e){
     console.log("Attempting to upload image")
     if (e.target.files && e.target.files[0]) {
         const file = e.target.files[0]
-        const formData = new FormData();
-        formData.append("file", file);
-        return await API.Common.uploadImage(formData);
+        return await this.uploadFile(file)
     }
+}
+
+Utils.uploadFile = async function(file){
+    const formData = new FormData();
+    formData.append("file", file);
+    return await API.Common.uploadImage(formData);
 }
 
 export default Utils;
