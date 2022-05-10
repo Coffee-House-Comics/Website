8e26a293206f63c0ec9da285553d5745cb18bb2d
taylor-giles <taylor.g1823@gmail.com>
2022-05-10 12:26:40 -0400

    Author card

--- src/Components/Cards/AuthorCard.js
+++ src/Components/Cards/AuthorCard.js
@@ -6,11 +6,11 @@ export default function AuthorCard(props) {
     let imgURL = props.img ? props.img : "https://coffeehousecomics.com/images/fetch/4752e287bba0c0f51a0ce1900.jpg"
 
     return (
-        <Grid direction="row" justifyContent="center" alignItems="center">
-            <Grid item xs={12}>
+        <Grid direction="column" justifyContent="center" alignItems="center">
+            <Grid item>
                 <Avatar src={imgURL} sx={{ width: "100px", height: "100px" }} />
             </Grid>
-            <Grid item xs={12}>
+            <Grid item>
                 <Typography variant="h6">
                     {name}
                 </Typography>
