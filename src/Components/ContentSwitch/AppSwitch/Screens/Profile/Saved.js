299726c53ff7c751cd86fc2829c4bb6cd78d0b82
Shaan Khan <contactshaankhan@gmail.com>
2022-05-10 13:14:25 -0400

    Update saved page rendering.

--- src/Components/ContentSwitch/AppSwitch/Screens/Profile/Saved.js
+++ src/Components/ContentSwitch/AppSwitch/Screens/Profile/Saved.js
@@ -3,22 +3,49 @@ import React from 'react'
 import { testStories } from '../../../../../App';
 import PostCard from '../../../../Cards/PostCard';
 import PostsSection, { PADDING_BTWN_CARDS } from '../../../../Cards/PostsSection';
+import API from '../../../../../API';
 
 export default function Saved(props) {
-    
+
     const { user } = props;
 
-    useEffect(() => {
+    const [toShow, setToShow] = React.useState([]);
+
+
+    React.useEffect(() => {
         async function getSaved() {
-            let resp = store.app==='Comics'? await API.Comic.saved() : await API.Story.saved()
+            let resp = store.app === 'Comics' ? await API.Comic.saved() : await API.Story.saved()
 
             console.log("Saved Page Ids:", resp.data.content)
+
+            setToShow(resp.data.content);
         }
         getSaved();
     }, [])
 
+    return (
+        <div style={{ padding: 40, paddingInline: 25 }}>
+            <Grid container direction="row" spacing={10}>
+                {/* Posts */}
+                <Grid item xs={12}>
+                    <Grid container direction="row" justifyContent="space-evenly">
+                        {toShow.map((post) => {
+                            return (
+                                <Grid item>
+                                    <PostCard post={post} />
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+
+    );
+
+
     return <Typography>Community Implementation not done.</Typography>
-    
+
     //TODO
     /**
      * Format:
