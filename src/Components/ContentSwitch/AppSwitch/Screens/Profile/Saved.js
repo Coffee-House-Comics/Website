c2fefa7a8a113926952dc49da360ae1970458eda
Shaan Khan <contactshaankhan@gmail.com>
2022-05-10 13:20:14 -0400

    Fix compile errors in saved.

--- src/Components/ContentSwitch/AppSwitch/Screens/Profile/Saved.js
+++ src/Components/ContentSwitch/AppSwitch/Screens/Profile/Saved.js
@@ -4,10 +4,13 @@ import { testStories } from '../../../../../App';
 import PostCard from '../../../../Cards/PostCard';
 import PostsSection, { PADDING_BTWN_CARDS } from '../../../../Cards/PostsSection';
 import API from '../../../../../API';
+import { GlobalStoreContext } from '../../../../../Store';
 
 export default function Saved(props) {
 
     const { user } = props;
+
+    const { store } = React.useContext(GlobalStoreContext);
 
     const [toShow, setToShow] = React.useState([]);
 
@@ -29,10 +32,10 @@ export default function Saved(props) {
                 {/* Posts */}
                 <Grid item xs={12}>
                     <Grid container direction="row" justifyContent="space-evenly">
-                        {toShow.map((post) => {
+                        {toShow.map((post, index) => {
                             return (
                                 <Grid item>
-                                    <PostCard post={post} />
+                                    <PostCard key={index} post={post} />
                                 </Grid>
                             )
                         })}
@@ -40,74 +43,5 @@ export default function Saved(props) {
                 </Grid>
             </Grid>
         </div>
-
     );
-
-
-    return <Typography>Community Implementation not done.</Typography>
-
-    //TODO
-    /**
-     * Format:
-     *    [{
-     *        name: String,
-     *        posts: [PostMetadata]
-     *    }]
-     */
-    let seriesContent = [{
-        name: "Series 1",
-        posts: testStories
-    },
-    {
-        name: "Series 2",
-        posts: testStories
-    },
-    {
-        name: "Series 3",
-        posts: testStories
-    },
-    {
-        name: "Series 4",
-        posts: testStories
-    },];
-
-    //TODO
-    /**
-     * Format:
-     *    [PostMetadata]
-     */
-    let nonSeriesContent = testStories;
-
-    //Build post sections for series
-    let seriesSections = seriesContent.map((series, index) =>
-        <PostsSection key={index} name={series.name} posts={series.posts} />
-    );
-
-    //Build section for non-series content
-    let nonSeriesSection = (nonSeriesContent) ?
-        <div style={{ alignText: "center" }}>
-            <hr style={{ margin: 30, marginBottom: 50 }} />
-            <Typography variant="h4" sx={{ marginBottom: "5px", marginTop: "20px" }}>
-                {"Individual Posts"}
-            </Typography>
-
-            <Grid container direction="row" justifyContent="flex-start" width="100%">
-                {
-                    nonSeriesContent.map((post, index) => {
-                        return <div key={index} style={{ padding: PADDING_BTWN_CARDS }}>
-                            <PostCard post={post} />
-                        </div>
-                    })
-                }
-            </Grid>
-        </div>
-
-        : ""
-
-    return (
-        <div style={{ height: "100%", overflowY: "scroll" }} >
-            {seriesSections}
-            {nonSeriesSection}
-        </div>
-    )
 }
