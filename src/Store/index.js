d15a64535664fb91b8477d2f48c22cbc91741ed4
Shaan Khan <shaan.khan@stonybrook.edu>
2022-05-16 19:00:46 -0400

    Redirect to metadata page when creating a new post now.

--- src/Store/index.js
+++ src/Store/index.js
@@ -303,7 +303,7 @@ function GlobalStoreContextProvider(props) {
                 if (response.status === 200) {
                     console.log("ID of the new Comic:", response.data, response.data.id);
 
-                    store.reRoute(types.TabType.CREATION.children.COMIC.fullRoute, response.data.id);
+                    store.reRoute(types.TabType.CREATION.children.METADATA.fullRoute, response.data.id);
                     return;
                 }
             }
